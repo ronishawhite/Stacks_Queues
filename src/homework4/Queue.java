994a11847185dfcc6e98c0f5ec8947d4b15c9f5d
ronishawhite <ronishawhite@gmail.com>
2016-10-30 20:39:52 -0400

    initializing
    initializing size, array, head, tail

--- src/homework4/Queue.java
+++ src/homework4/Queue.java
@@ -1,10 +1,17 @@
 package homework4;
 
 public class Queue {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int size;
+	private int[] array;
+	private int head;
+	private int tail;
+	
+	
+	public Queue(int size)
+	{
+		this.size=size;
+		array=new int[size+1];	//the queue is of size n-1 when the array is of size n
+		this.head=0;
+		this.tail=this.head;	//the queue is empty when initialized, Q.head=Q.tail=0;
 	}
-
-}
+	
