9b5ce0d2d65d6c2256d4f0d91c03bbe88d585320
ronishawhite <ronishawhite@gmail.com>
2016-10-30 20:33:33 -0400

    intialization
    size, top and array is initialized

--- src/homework4/Stack.java
+++ src/homework4/Stack.java
@@ -1,10 +1,15 @@
 package homework4;
 
 public class Stack {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private int size;	//size of the stack
+	private int top;
+	private int[] array;
+	
+	public Stack(int size)
+	{
+		this.size=size;
+		this.top=-1;	
+		array=new int[size];
 	}
-
-}
+	
